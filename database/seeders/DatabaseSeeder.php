aa62617d5b477eafbd6dd52b01fb11a538efc92e
Hinata Asuka <uj222005@cc.seikei.ac.jp>
2025-01-22 06:28:53 +0000

    seedingupload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,12 +21,12 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            // AgeSeeder::class,
-            // UserSeeder::class,
+            AgeSeeder::class,
+            UserSeeder::class,
             // PostSeeder::class,
             AreaSeeder::class,
             ConditionSeeder::class,
-            // WorkSeeder::class,
+            WorkSeeder::class,
            //SupporterSeeder::class,
          ]);
     }
